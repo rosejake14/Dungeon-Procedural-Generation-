b0c23e3376203de308cd08af4802d643680e723e
rosejake14 <jake.c.rose03@gmail.com>
2023-11-26 21:10:32 +0000

    Rooms spawn procedurally amazingly now. Redid everything.

---
+++ Source/PCGTesting/Round2/Generator.cpp
@@ -0,0 +1,314 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PCGTesting/Round2/Generator.h"
+
+#include "Components/InstancedStaticMeshComponent.h"
+#include "Math/RandomStream.h"
+
+// Sets default values
+AGenerator::AGenerator()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>("Root");
+	RootComponent = Root;
+
+	FloorISMC = CreateDefaultSubobject<UInstancedStaticMeshComponent>("FloorISMC");
+	FloorISMC->SetupAttachment(Root);
+
+	WallISMC = CreateDefaultSubobject<UInstancedStaticMeshComponent>("WallISMC");
+	WallISMC->SetupAttachment(Root);
+	
+	
+}
+
+// Called when the game starts or when spawned
+void AGenerator::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+}
+
+// Called every frame
+void AGenerator::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AGenerator::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	FloorISMC->ClearInstances();
+	WallISMC->ClearInstances();
+	PreviousLocation = FVector(0,0,0);
+	
+	if(NewSeed == true) //On click randomise and set new seed;
+	{
+		NewSeed = false;
+		Seed = rand () % 90000;
+		Stream.Initialize(Seed);
+	}
+	Stream.Initialize(Seed); //Manually update seed;
+	GenerateMap();
+}
+
+void AGenerator::GenerateMap()
+{
+	
+	FVector LocationOut;
+	FVector Extents;
+	TMap<FVector, FVector> Rooms;
+	TArray<FVector> FloorTilesIn;
+	UE_LOG(LogTemp, Warning, TEXT("Generating Map..."));
+	for(int i = 0; i < RoomCount; i++)
+	{
+		if(i == 0) //Generates first location of Actor 0,0,0.
+		{
+			MakeFloor(PreviousLocation, FloorTilesIn, LocationOut, Extents);
+			FloorTiles = FloorTilesIn;
+			Rooms.Add(LocationOut, Extents);
+			UE_LOG(LogTemp, Warning, TEXT("Room Added"));
+		}else
+		{
+			bool Valid;
+			FVector NextLocation;
+			FindNextLocation(Valid, NextLocation);
+			if(Valid == true)
+			{
+				MakeFloor(NextLocation, FloorTilesIn, LocationOut, Extents);
+				FloorTiles.Append(FloorTilesIn);
+				Rooms.Add(LocationOut, Extents);
+				UE_LOG(LogTemp, Warning, TEXT("Another Room Added"));
+				PreviousLocation = NextLocation;
+			}
+		}
+	}
+	SpawnTiles();
+}
+
+void AGenerator::MakeFloor(FVector Location, TArray<FVector> &FloorTilesIn, FVector &LocationOut, FVector &Extents) 
+{
+	//Get Random X and Y Lengths & Make array of Floor Tiles from Root Position.
+	FVector RootLocation = Location;
+	int32 OutX = Stream.RandRange(RoomSizeMin, RoomSizeMax);
+	int32 OutY = Stream.RandRange(RoomSizeMin, RoomSizeMax);
+	Working = true;
+	int32 Area = (OutX * OutY) - 1;
+	TArray<FVector> Tiles;
+	TArray<int32> ExtentsX;
+	TArray<int32> ExtentsY;
+	TArray<FVector> ConnectedTiles;
+	TArray<FVector> TilesCopy;
+	bool TileFound = true;
+	int32 LoopCount = 0;
+	
+	UE_LOG(LogTemp, Warning, TEXT("Adding Tiles..."));
+	for(int i = 0; i <= Area; i++)
+	{
+		Tiles.Add(FVector((i / OutY) + RootLocation.X, (i % OutY) + RootLocation.Y, RootLocation.Z));
+	}
+	if(isCulling)
+	{
+		while (Working)
+		{
+			if(LoopCount <= MaxLoops) //Continue looping while verifying floor.
+			{
+				//Randomly Remove Tiles From Floor.
+				TilesCopy = Tiles;
+				int ClampedCullingValue = FMath::Clamp(Stream.RandRange(FloorCullingMin,FloorCullingMax) - 1, 0, TilesCopy.Num()/4); // %4 so only a max of a 1/4 will be culled.
+				for(int i = 0; i < ClampedCullingValue; i++)
+				{
+					int RandomIndex = Stream.RandRange(0,TilesCopy.Num() - 1);
+					TilesCopy.RemoveAt(RandomIndex);
+				}
+				ConnectedTiles.Empty();
+				ConnectedTiles.Add(TilesCopy[0]);
+				while(TileFound)
+				{ // Loop through tiles testing if adjacent tiles are present; if so, add to connected Tiles.
+					TileFound = false;
+					bool isInArray;
+					FVector TileLocation;
+					for (auto Tile : ConnectedTiles)
+					{
+						for(int i = 0; i < 3; i++)
+						{
+							TestRelativeLocation(Tile, TilesCopy,1,0,TileLocation,isInArray);
+							if(isInArray)
+							{
+								ConnectedTiles.Add(TileLocation);
+								TilesCopy.Remove(TileLocation);
+								TileFound = true;
+							}
+							TestRelativeLocation(Tile, TilesCopy,0,1,TileLocation,isInArray);
+							if(isInArray)
+							{
+								ConnectedTiles.Add(TileLocation);
+								TilesCopy.Remove(TileLocation);
+								TileFound = true;
+							}
+							TestRelativeLocation(Tile, TilesCopy,-1,0,TileLocation,isInArray);
+							if(isInArray)
+							{
+								ConnectedTiles.Add(TileLocation);
+								TilesCopy.Remove(TileLocation);
+								TileFound = true;
+							}
+							TestRelativeLocation(Tile, TilesCopy,0,-1,TileLocation,isInArray);
+							if(isInArray)
+							{
+								ConnectedTiles.Add(TileLocation);
+								TilesCopy.Remove(TileLocation);
+								TileFound = true;
+							}
+							
+						}
+					}
+					
+				}
+				if(ConnectedTiles.Num() > RoomSizeMin * RoomSizeMax) //Is connected Tiles Larger than area?:
+					{
+					Working = false;
+					}else
+					{ //If its not, try again.
+						Working = true;
+						++LoopCount;
+					}
+			}else
+			{ //Fail out without culling any tiles.
+				Working = false;
+				ConnectedTiles = Tiles;
+			}
+		}
+	}else
+	{
+		ConnectedTiles = Tiles;
+	}
+	
+
+	for (auto Tile : FloorTilesIn)
+	{
+		ExtentsX.Add(Tile.X);
+		ExtentsY.Add(Tile.Y);
+	}
+	
+	Extents = FVector(ExtentsX.Max(), ExtentsY.Max(), 0);
+	LocationOut = RootLocation;
+	FloorTilesIn = ConnectedTiles;
+	UE_LOG(LogTemp, Warning, TEXT("Returning Floor Values..."));
+}
+
+void AGenerator::SpawnTiles()
+{
+	FVector Location;
+	FQuat Rotation;
+	bool isFloorF, isFloorR, isFloorD, isFloorL;
+	UE_LOG(LogTemp, Warning, TEXT("Preparing Tiles."));
+	
+	for (auto Tile : FloorTiles)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Spawning Tiles."));
+		FloorISMC->AddInstance(FTransform(Tile * Scale));
+		
+		for(int i = 0; i < 3; i++)
+		{
+			TestRelativeLocation(Tile, FloorTiles, 1, 0, Location, isFloorF);
+			TestRelativeLocation(Tile, FloorTiles,0, 1, Location, isFloorR);
+			TestRelativeLocation(Tile, FloorTiles,-1, 0, Location, isFloorD);
+			TestRelativeLocation(Tile, FloorTiles, 0, -1, Location, isFloorL);
+			if(!isFloorF)
+			{
+				Rotation = FQuat::MakeFromEuler(FVector(0,0,90));
+				FVector FloorLocation = FVector(Tile)  * Scale;
+				FloorLocation.X += Scale;
+				WallISMC->AddInstance(FTransform(Rotation, FloorLocation));
+			}
+			if(!isFloorR)
+			{
+				Rotation = FQuat::MakeFromEuler(FVector(0,0,0));
+				FVector FloorLocation = FVector(Tile)  * Scale;
+				FloorLocation.Y += Scale;
+				WallISMC->AddInstance(FTransform(Rotation, FloorLocation));
+			}
+			if(!isFloorD)
+			{
+				Rotation = FQuat::MakeFromEuler(FVector(0,0,-90));
+				FVector FloorLocation = FVector(Tile)  * Scale;
+				FloorLocation.Y += Scale;
+				WallISMC->AddInstance(FTransform(Rotation, FloorLocation));
+			}
+			if(!isFloorL)
+			{
+				Rotation = FQuat::MakeFromEuler(FVector(0,0,180));
+				FVector FloorLocation = FVector(Tile)  * Scale;
+				FloorLocation.X += Scale;
+				WallISMC->AddInstance(FTransform(Rotation, FloorLocation));
+			}
+		}
+	}
+}
+
+void AGenerator::FindNextLocation(bool &Valid, FVector &NewLocationtoSpawn)
+{
+	TArray<int32> Directions;
+	bool Searching = true;
+	int32 TestIndex;
+	for(int i = 0; i < 4; i++)
+	{
+		Directions.Add(i);
+	}
+	while (Searching)
+	{
+		if(Directions.Last() >= 0) //Check for options available.
+		{
+			TestIndex = Stream.RandRange(0, Directions.Last());
+			int32 addedInput;
+			FVector Location;
+			if(Merging){addedInput = RoomSizeMax;}else{addedInput = RoomSizeMax+1;}
+			switch(TestIndex)
+			{
+			case(0):
+				TestRelativeLocation(PreviousLocation, FloorTiles,addedInput,0,Location,isFloorTile);
+				break;
+			case(1):
+				TestRelativeLocation(PreviousLocation, FloorTiles,0,addedInput,Location,isFloorTile);
+				break;
+			case(2):
+				TestRelativeLocation(PreviousLocation, FloorTiles,-addedInput,0,Location,isFloorTile);
+				break;
+			case(3):
+				TestRelativeLocation(PreviousLocation, FloorTiles,0,-addedInput,Location,isFloorTile);
+				break;
+					
+			default: ;
+			}
+			if(isFloorTile == true)
+			{
+				Directions.Remove(TestIndex);
+				Searching = true;
+			}else
+			{
+				Searching = false;
+				ValidTileLocation = true;
+				NewLocation = Location;
+			}
+		}else //Failed to Find New Empty Room Location.
+		{
+			Searching = false;
+			ValidTileLocation = false;
+		}
+	}
+	Valid = ValidTileLocation;
+	NewLocationtoSpawn = NewLocation;
+}
+
+void AGenerator::TestRelativeLocation(FVector ReferenceLocation,TArray<FVector> TestArray, int32 X, int32 Y, FVector &Location, bool &isPresentTile)
+{ //Check if Tile relative to Reference has an existing floor tile.
+	Location = FVector(ReferenceLocation.X + X,ReferenceLocation.Y + Y,ReferenceLocation.Z);
+	isPresentTile = TestArray.Contains(Location);
+
+	//1:13:51
+}
---
+++ Source/PCGTesting/Round2/Generator.h
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Generator.generated.h"
+
+UCLASS()
+class PCGTESTING_API AGenerator : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGenerator();
+	virtual void Tick(float DeltaTime) override;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+	void GenerateMap();
+	void MakeFloor(FVector Location, TArray<FVector> &FloorTilesIn, FVector &LocationOut, FVector &Extents);
+	void SpawnTiles();
+	void FindNextLocation(bool &Valid, FVector &NewLocationtoSpawn);
+	void TestRelativeLocation(FVector ReferenceLocation, TArray<FVector> TestArray, int32 X, int32 Y, FVector &Location, bool &isPresentTile);
+
+	UPROPERTY(EditAnywhere)
+	int32 Seed;
+	UPROPERTY(EditAnywhere, Category = " ")
+	int32 RoomSizeMin;
+	UPROPERTY(EditAnywhere, Category = " ")
+	int32 RoomSizeMax;
+	UPROPERTY(EditAnywhere, Category = " ")
+	int32 RoomCount;
+	UPROPERTY(EditAnywhere)
+	int32 FloorCullingMin; //Holes in Rooms
+	UPROPERTY(EditAnywhere)
+	int32 FloorCullingMax;
+	UPROPERTY(EditAnywhere, Category = " ")
+	bool isCulling;
+	UPROPERTY(EditAnywhere)
+	int32 BranchingThreshold;
+	UPROPERTY(EditAnywhere);
+	int32 MaxLoops = 10;
+	UPROPERTY(EditAnywhere, Category = " ");
+	bool NewSeed;
+	UPROPERTY(EditAnywhere);
+	FRandomStream Stream;
+	UPROPERTY(EditAnywhere);
+	float Scale;
+	UPROPERTY(EditAnywhere)
+	float BranchingChance;
+	UPROPERTY(EditAnywhere)
+	bool Merging;
+	UPROPERTY(EditAnywhere)
+	bool Branching;
+	UPROPERTY(EditAnywhere)
+	TArray<FVector> FloorTiles;
+	UPROPERTY(EditAnywhere)
+	UInstancedStaticMeshComponent* FloorISMC;
+	UPROPERTY(EditAnywhere)
+	UInstancedStaticMeshComponent* WallISMC;
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* Root = nullptr;
+	UPROPERTY(EditAnywhere)
+	bool Working = true;
+	UPROPERTY(EditAnywhere)
+	FVector PreviousLocation;
+
+	bool isFloorTile;
+	bool ValidTileLocation = false;
+	FVector NewLocation;
+	
+
+};
+
+
